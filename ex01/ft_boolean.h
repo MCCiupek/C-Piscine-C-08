6b2aa6d0715f8c397a41a714d25494c5741eb2fb
MCCiupek <51337012+MCCiupek@users.noreply.github.com>
2019-06-24 20:28:09 +0200

    Add files via upload

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mciupek <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/24 11:29:13 by mciupek           #+#    #+#             */
+/*   Updated: 2019/06/24 16:43:48 by mciupek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define EVEN(x) x % 2 == 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+
+typedef int		t_bool;
+
+#endif
